4fdcd2a4a01fc069a4f6433031cdb628c3a6e3a8
豫风 <991810133@qq.com>
2018-07-02 22:23:28 +0800

    增加自定义导航栏，解决隐藏导航栏手势失效问题。

---
+++ HHTransitionDemo/HHBaseNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  HHBaseNavigationController.h
+//  BaseTabBar
+//
+//  Created by 豫风 on 2017/6/23.
+//  Copyright © 2017年 豫风. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HHBaseNavigationController : UINavigationController
+
+@end
